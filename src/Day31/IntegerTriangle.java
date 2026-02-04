fe82bf22eac602e911fdb039eba3e9c25810c18e
farmer0010 <zmqkf16@naver.com>
2026-02-05 08:43:44 +0900

    Day31 Pratice

---
+++ src/Day31/IntegerTriangle.java
@@ -0,0 +1,28 @@
+package Day31;
+
+class IntegerTriangle {
+    public int solution(int[][] triangle) {
+        for(int i = 1; i < triangle.length; i++){
+            for(int j = 0; j < triangle[i].length; j++){
+                if(j == 0)
+                {
+                    triangle[i][j] += triangle[i-1][j];
+                }
+                else if(j == triangle[i].length-1)
+                {
+                    triangle[i][j] += triangle[i-1][j-1];
+                }
+                else
+                {
+                    triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
+                }
+            }
+        }
+        int answer = 0;
+        for(int i : triangle[triangle.length-1])
+        {
+            answer = Math.max(answer, i);
+        }
+        return answer;
+    }
+}
---
+++ src/Day31/Tiling3xN.java
@@ -0,0 +1,20 @@
+package Day31;
+
+class Tiling3xN {
+    public int solution(int n) {
+        if(n % 2 != 0)
+            return 0;
+        long[] dp = new long[n+1];
+        dp[0] = 1;
+        dp[2] = 3;
+
+        for(int i = 4; i <= n; i+=2)
+        {
+            dp[i] = dp[i -2]*3;
+            for(int j = i-4; j >= 0; j-=2)
+                dp[i] += dp[j]*2;
+            dp[i] %= 1000000007;
+        }
+        return (int)dp[n];
+    }
+}
